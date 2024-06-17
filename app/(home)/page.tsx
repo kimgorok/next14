8955d0dff452cc581938fbe407340a9d96c3d2d5
kimgorok <kjyse2@naver.com>
2024-06-17 19:15:26 +0900

    🌱(홈) 포켓몬 이미지 추가

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,23 +1,50 @@
-import React from "react";
+"use client";
 
-export const metadata = {
-  title: "Home",
-};
+import React, { useEffect, useState } from "react";
+
 export const API_URL = "https://pokeapi.co/api/v2/pokemon?limit=151";
 
+// 순수 함수로 데이터 가져오기 함수 정의
 async function getPokemons() {
   const response = await fetch(API_URL);
   const json = await response.json();
   return json.results;
 }
 
-export default async function HomePage() {
-  const pokemons = await getPokemons();
-  console.log(pokemons);
+// 각 포켓몬의 세부 정보 가져오기 함수 정의
+async function getPokemonDetails(url) {
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
+}
+
+export default function HomePage() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const basicPokemons = await getPokemons();
+      const detailedPokemons = await Promise.all(
+        basicPokemons.map(async (pokemon) => {
+          const details = await getPokemonDetails(pokemon.url);
+          return {
+            name: details.name,
+            image: details.sprites.front_default,
+          };
+        })
+      );
+      setPokemons(detailedPokemons);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="w-full flex-col items-center justify-center h-full">
-      {pokemons.map((pokemons, index) => (
-        <div key={index}>{pokemons.name}</div>
+      {pokemons.map((pokemon, index) => (
+        <div key={index}>
+          <img src={pokemon.image} alt={pokemon.name} />
+          <p>{pokemon.name}</p>
+        </div>
       ))}
     </div>
   );
